d5cc445ab7914ba46ec06e947d4678366911d727
Besar <56453738+kapll@users.noreply.github.com>
2020-11-02 19:18:46 -0500

    Update LectureTimeCollisionException.java

--- LectureTimeCollisionException.java
+++ LectureTimeCollisionException.java
@@ -1,6 +1,3 @@
-//Besar Kapllani - 500943601
-
-
 public class LectureTimeCollisionException extends RuntimeException {
     public LectureTimeCollisionException(){}
     public LectureTimeCollisionException(String message){
